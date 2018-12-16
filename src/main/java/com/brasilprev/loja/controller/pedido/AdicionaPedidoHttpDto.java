79867a1c8aa8091e2a7bb46783d144d56845891d
Fernando Cesar <fernandoslv16@gmail.com>
2018-12-16 15:04:05 -0300

    Ajustando serviço de criação de pedido para que este crie também seus itens do pedido

--- src/main/java/com/brasilprev/loja/controller/pedido/AdicionaPedidoHttpDto.java
+++ src/main/java/com/brasilprev/loja/controller/pedido/AdicionaPedidoHttpDto.java
@@ -1,5 +1,8 @@
 package com.brasilprev.loja.controller.pedido;
 
+import java.util.HashMap;
+
 public class AdicionaPedidoHttpDto {
     public Long idDoCliente;
+    public HashMap<Long, Integer> produtos;
 }
--- src/main/java/com/brasilprev/loja/servico/pedido/ServicoDeCriacaoDePedido.java
+++ src/main/java/com/brasilprev/loja/servico/pedido/ServicoDeCriacaoDePedido.java
@@ -4,30 +4,45 @@ import com.brasilprev.loja.controller.ConfirmacaoDeSucesso;
 import com.brasilprev.loja.controller.pedido.AdicionaPedidoHttpDto;
 import com.brasilprev.loja.dominio.Cliente;
 import com.brasilprev.loja.dominio.Pedido;
+import com.brasilprev.loja.dominio.PedidoItem;
+import com.brasilprev.loja.dominio.Produto;
 import com.brasilprev.loja.dominio.excecao.ExcecaoDeRegraDeNegocio;
 import com.brasilprev.loja.infra.produto.ClienteRepositorio;
+import com.brasilprev.loja.infra.produto.ProdutoRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class ServicoDeCriacaoDePedido implements IServicoDeCriacaoDePedido {
     private ClienteRepositorio clienteRepositorio;
+    private ProdutoRepositorio produtoRepositorio;
 
     @Autowired
-    public ServicoDeCriacaoDePedido(ClienteRepositorio clienteRepositorio) {
+    public ServicoDeCriacaoDePedido(ClienteRepositorio clienteRepositorio, ProdutoRepositorio produtoRepositorio) {
         this.clienteRepositorio = clienteRepositorio;
+        this.produtoRepositorio = produtoRepositorio;
     }
 
     @Override
     public ConfirmacaoDeSucesso criar(AdicionaPedidoHttpDto adicionaPedidoHttpDto) {
         Optional<Cliente> clienteEncontrado = clienteRepositorio.findById(adicionaPedidoHttpDto.idDoCliente);
-
         clienteEncontrado.orElseThrow(() -> new ExcecaoDeRegraDeNegocio("Cliente não encontrado."));
 
         Pedido pedido = Pedido.criar(clienteEncontrado.get());
 
+        criarItensDoPedido(adicionaPedidoHttpDto.produtos, pedido);
+
         return new ConfirmacaoDeSucesso(pedido.getId());
+    }
+
+    private void criarItensDoPedido(HashMap<Long, Integer> produtos, Pedido pedido) {
+        produtos.forEach((idDoProduto, quantidade) -> {
+            Optional<Produto> produto = produtoRepositorio.findById(idDoProduto);
+            produto.orElseThrow(() -> new ExcecaoDeRegraDeNegocio("Produto não encontrado."));
+
+            PedidoItem.criar(produto.get(), pedido, quantidade, produto.get().getPreco());
+        });
     }
 }
